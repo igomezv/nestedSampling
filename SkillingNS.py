fa9ab3a1873d2e2121aafb362ef202baa558c792
igomezv <neorelativista@gmail.com>
2020-04-30 02:08:07 -0500

    z->logz and stopping criterion

--- SkillingNS.py
+++ SkillingNS.py
@@ -5,9 +5,9 @@ import os.path
 from scipy.special import logsumexp
 import sys
 
+
 class SkillingNS:
-    def __init__(self, logLike, priorTransform, nDims, bounds, nlivepoints=50,
-                 accuracy=0.5, **kwargs):
+    def __init__(self, logLike, priorTransform, nDims, bounds, nlivepoints=50):
         """
             Parameters
             -----------
@@ -27,24 +27,23 @@ class SkillingNS:
         self.nDims = nDims
         self.bounds = bounds
         self.nlivepoints = nlivepoints
-        self.accuracy = accuracy
-        self.outputname = "outputSamples"
         self.accepted = 0
         self.rejected = 0
         # self.ncall = 0
 
-    def sampler(self):
+    def sampler(self, accuracy=0.01, outputname="nestedOutput"):
         vectors = []
         # vector of loglikes
         loglikes = []
         # list for dead values
         deadpoints = []
         deadloglikes = []
-        if os.path.isfile(self.outputname+'.txt'):
-    		print("File exist! Please choose another name or move the existing file.")
-    		sys.exit(1)
-    	else:
-        	f = open(self.outputname + '.txt', 'a+')
+        if os.path.isfile(outputname + '.txt'):
+            print("Output file exists! Please choose another"
+                  " name or move the existing file.")
+            sys.exit(1)
+        else:
+            f = open(outputname + '.txt', 'a+')
 
         for i in range(self.nlivepoints):
             vectors.append(self.priorTransform(np.random.rand(self.nDims, ),
@@ -63,16 +62,16 @@ class SkillingNS:
         # evidence is z
         # print("bounds", self.bounds)
         # What is a good initial value for logz?
-        # logz = -np.inf
-        # logz = 1e-50
+        logz = -np.inf
+        # logz = -1e50
 
-        z = 0
+        # z = 0
         # initial prior mass is x0 = 1
         # x_prev = 1 -> logx =0
-        logx = 0
+        plogx = 0
         # x_prev = 1
-        #x_current = 1
-        clogx = 0
+        # x_current = 1
+        # clogx = 0
 
         # h = 0
         # j iterations
@@ -88,39 +87,35 @@ class SkillingNS:
             deadloglikes.append(lowloglike)
             deadpoints.append(lowpoint)
             # new prior mass X_i in the paper [crude]
-            
             # x_current = np.exp(-(i + 1) / self.nlivepoints)
-            clogx = -(i + 1) / self.nlivepoints
-            # xi = -i / self.nlivepoints
-            print("logX_i : {}".format(clogx))
-            # wi simple (no trapezoidal)
-            #wi = x_prev - x_current
-            # Error! use scipy
-            # logwi = np.logaddexp(logx, clogx)
-            logwi = logsumexp([logx, clogx], b = [1, -1])
-            
-            print("logw_i : {}".format(logwi))
-            logLwi = lowloglike - logwi
+            clogx = -(i + 1) / float(self.nlivepoints)
+            print("logX_i-1: {}, clogXi: {}".format(plogx, clogx))
+            logwi = logsumexp([plogx, clogx], b=[1, -1])
+            print("logw_i: {}".format(logwi))
+            logLwi = lowloglike + logwi
             # z increment
-            z += np.exp(lowloglike) * np.exp(logwi)
-            # logz = np.logaddexp(logz, logLwi)
-            print("lowpoint {}".format(lowpoint))
+            logz = logsumexp([logz, logLwi])
+            print("lowpoint: {}".format(lowpoint))
             # newpoint, newlike = self.generate_point(lowpoint, lowloglike)
             newpoint, newlike = self.metropolis(lowpoint, lowloglike, 500)
-
-            print("new point {}".format(newpoint))
+            print("new point: {}".format(newpoint))
             df_live.iloc[0] = newpoint, newlike
-            self.print_func(df_live, np.log(z))
-            # x_prev = x_current
-            logx = clogx
+            self.print_func(df_live, logz)
+            plogx = clogx
             strnewpoint = str(newpoint).lstrip('[').rstrip(']')
-            f.write("{} {} {}".format(logwi, newlike, strnewpoint))
-
+            f.write("{} {} {}\n".format(logwi, newlike, strnewpoint))
+            # What is a good value for f?
+            stop = self.stoppingCriteria(df_live['loglikes'].values, clogx, logz, f=accuracy)
+            if stop:
+                break
         f.close()
-
+        lgsumexplglikes = logsumexp(df_live['loglikes'].values)
+        logzsum = lgsumexplglikes + clogx - np.log(float(self.nlivepoints))
+        logz = logsumexp([logz, logzsum])
+        print("logz : {}".format(logz))
         # logz = np.logaddexp(logz, np.logaddexp(df_live['loglikes'].values) + logx - np.log(self.nlivepoints))
         # logz = np.logaddexp(logz, np.sum(df_live['loglikes'].values + logx - np.log(self.nlivepoints)))
-        z += np.sum(np.exp(df_live['loglikes'].values)) * np.exp(clogx) / self.nlivepoints
+        # z += np.sum(np.exp(df_live['loglikes'].values)) * np.exp(clogx) / self.nlivepoints
 
         df_dead = pd.DataFrame()
         df_dead['points'] = deadpoints
@@ -173,36 +168,46 @@ class SkillingNS:
         print("Parameter estimation : {}".format(highestpoint))
         print("logLikelihood : {}".format(highestlike))
         print("log(Z) : {}".format(logz))
-	
+
     def metropolis(self, ctheta, cloglike, iter):
-        logf = lambda x : self.logLike(x) + self.logPrior(x)       
+        logf = lambda x: self.logLike(x) + self.logPrior(x)
         samples = np.zeros((iter, 2))
         self.accepted = 0
         self.rejected = 0
-        
+
         for i in range(iter):
             vstar = np.array(ctheta) + np.random.normal(size=len(ctheta))
-	    	# logLikeStar = self.logLike(vstar)
+            # logLikeStar = self.logLike(vstar)
             r = np.random.rand()
             if logf(vstar) - logf(ctheta) > np.log(r):
                 ctheta = vstar
                 cloglike = self.logLike(ctheta)
-                self.accepted+=1
+                self.accepted += 1
             else:
-                self.rejected+=1
-            
+                self.rejected += 1
+
         samples[i] = np.array(ctheta)
         return np.array(ctheta), cloglike
 
     def logPrior(self, theta):
+        flag = True
         for i, bound in enumerate(self.bounds):
             if bound[0] < theta[i] < bound[1]:
-                flag = True
+                pass
             else:
                 flag = False
-                break  
-        
-        if flag == True:
+                break
+        if flag:
             return 0.0
         else:
             return -np.inf
+
+    def stoppingCriteria(self, loglikes, logx, logz, f=0.01):
+        maxloglike = np.max(loglikes)
+        if maxloglike + logx < logz + np.log(f):
+            print("Stopping Criteria reached!")
+            return True
+        else:
+            print("maxloglike+logx: {}, logz+logf: {}".format(
+                maxloglike + logx, logz + np.log(f)))
+            return False
