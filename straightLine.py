d13a849b5a6a92c8b23aef574111855d5f82a594
igomezv <neorelativista@gmail.com>
2020-05-29 00:01:59 -0500

    comparing with dynesty

--- straightLine.py
+++ straightLine.py
@@ -1,46 +1,9 @@
-# from SkillingNS import SkillingNS
-# from SkillingNSv01 import SkillingNS
-from nestedSampling import SkillingNS
+from nestedSampling import nestedSampling
 import numpy as np
-# import dynesty
+import dynesty
+from math import *
+import random
 # #### We need a prior Transform, logLike and Theory##########
-
-names = ['m', 'c']
-LaTeXnames = ['m_1', 'c_1']
-bounds = [[0, 10], [-2, 6]]
-
-def priorTransform(theta):
-    """
-    This is a common prior transform (flat priors).
-
-    Parameters:
-        theta  : is a random vector with de dimensionality of the model.
-        bounds : list of lists of lower and higher bound for each parameter.
-    """
-    points = []
-    bounds = [[0, 10], [-2, 6]]
-    # When theta 0-> append bound[0], if theta 1-> append bound[1]
-    for c, bound in enumerate(bounds):
-        points.append(theta[c]*(bound[1]-bound[0])+bound[0])
-
-    # At this moment, np.array(priors) has shape (dims,)
-    # print("Prior transform : {}".format(np.array(priors)))
-    return points
-
-sigma = 0.5
-
-def logLike(theta):
-    """
-    This function is the logLikelihood.
-    Parameters:
-        theta : is a random vector with lenght as the free parameters of the model.
-    """
-    m, c = theta  # unpack the parameters
-
-    chisq = np.sum(((data-theory(x, m, c))/sigma)**2)
-
-    return -0.5*chisq
-
 
 def theory(x, m, c):
     """
@@ -53,6 +16,56 @@ def theory(x, m, c):
         c (float): the y-intercept of the line
     """
     return m*x+c
+
+names = ['m', 'c']
+LaTeXnames = ['m_1', 'c_1']
+bounds = [[0, 5], [-2, 6]]
+
+m = 3.5  # gradient of the line
+c = 1.2  # y-intercept of the line
+
+M = 1000
+xmin = 0.
+xmax = 10.
+sigma = 0.5
+
+stepsize = (xmax-xmin)/M
+np.random.seed(0)
+x = np.arange(xmin, xmax, stepsize)
+
+# create the data - the model plus Gaussian noise
+data = theory(x, m, c) + sigma*np.random.randn(M)
+
+def priorTransform(theta):
+    """
+    This is a common prior transform (flat priors).
+
+    Parameters:
+        theta  : is a random vector with de dimensionality of the model.
+        bounds : list of lists of lower and higher bound for each parameter.
+    """
+    points = []
+    # bounds = [[0, 10], [-2, 6]]
+    # When theta 0-> append bound[0], if theta 1-> append bound[1]
+    for c, bound in enumerate(bounds):
+        points.append(theta[c]*(bound[1]-bound[0])+bound[0])
+
+    # At this moment, np.array(priors) has shape (dims,)
+    # print("Prior transform : {}".format(np.array(priors)))
+    return points
+
+
+def logLike(theta):
+    """
+    This function is the logLikelihood.
+    Parameters:
+        theta : is a random vector with lenght as the free parameters of the model.
+    """
+    m, c = theta  # unpack the parameters
+
+    chisq = np.sum(((data-theory(x, m, c))/sigma)**2)
+
+    return -0.5*chisq
 
 
 def saveDynestyChain(result, outputname):
@@ -72,41 +85,23 @@ def saveDynestyChain(result, outputname):
         nrow = " ".join(row.split())
         f.write(nrow + '\n')
 
+nlivepoints = 100
 
-# ##########create some data#######################################
-# set the true values of the model parameters for creating the data
-# m = 4.2  # gradient of the line
-# c = 2.1  # y-intercept of the line
-m = 3.5  # gradient of the line
-c = 1.2  # y-intercept of the line
+s = nestedSampling(logLike, priorTransform, nlive=200, ndims=2, maxiter=30000)
+s.sampling()
+# results = nested_sampling(n, max_iter, sample_from_prior, explore, priorTransform)
+# process_results(results)
 
-# set the "predictor variable"/abscissa
-M = 1000
-xmin = 0.
-xmax = 10.
+dysampler = dynesty.NestedSampler(logLike, priorTransform, 2,
+                                  bound='single', sample='unif', nlive=100)
+dysampler.run_nested(dlogz=0.01)
+dyresults = dysampler.results
+saveDynestyChain(dyresults, "dynestySamples")
 
-stepsize = (xmax-xmin)/M
-np.random.seed(0)
-x = np.arange(xmin, xmax, stepsize)
-# create the data - the model plus Gaussian noise
-
-data = theory(x, m, c) + sigma*np.random.randn(M)
-
-# bounds = [[bound_inf_m, bound_sup_m], [bound_inf_c, bound_sup_c]]
-
-# ##################Run Nested Sampling #########################
-nDims = 2
-
-#
-sampler = SkillingNS(logLike, priorTransform, nDims, bounds=bounds,
-                     nlivepoints=50)
-#
-sampler.sampler(accuracy=0.1, maxiter=2000, outputname="line")
+dysampler = dynesty.NestedSampler(logLike, priorTransform, 2,
+                                  bound='multi', sample='unif', nlive=100)
+dysampler.run_nested(dlogz=0.01)
+dyresults = dysampler.results
+saveDynestyChain(dyresults, "dynestySamples2")
 
 
-#dynesty
-# dysampler = dynesty.NestedSampler(logLike, priorTransform, nDims,
-#                                   bound='single', sample='unif', nlive=100)
-# dysampler.run_nested(dlogz=0.01)
-# dyresults = dysampler.results
-# saveDynestyChain(dyresults, "dynestySamplessingle")
