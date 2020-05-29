3ed134983935dbb935c16e450dfa1306aaf43ba0
igomezv <neorelativista@gmail.com>
2020-05-28 19:53:25 -0500

    testing code as appendix

--- nestedSampling.py
+++ nestedSampling.py
@@ -1,193 +1,153 @@
-# Nested sampling based on Skilling (2009)
-import numpy as np
-import pandas as pd
-import os.path
 from scipy.special import logsumexp
-import sys
-import random
-from math import floor
+import numpy as np
 
-class SkillingNS:
-    def __init__(self, logLike, priorTransform, nDims, bounds, nlivepoints=50):
-        """
-            Parameters
-            -----------
-            logLike : log-likelihood function
-            priorTransform :
-                                        priorTransform function that maps(0, 1)
-                        -> (bound_inf, bound_sup)
-            nDims : int # dimensions
-            accuracy : float
-            #dim is the dimension or the number of free parameters
+class Object:
+    def __init__(self):
+        self.u=None     # Uniform-prior controlling parameter for x
+        self.v=None     # Uniform-prior controlling parameter for y
+        self.x=None     # Geographical easterly position of lighthouse
+        self.y=None     # Geographical northerly position of lighthouse
+        self.logL=None  # logLikelihood = ln Prob(data | position)
+        self.logWt=None # log(Weight), adding to SUM(Wt) = Evidence Z
 
-            **kwargs:
-
-        """
+class nestedSampling:
+    def __init__(self, logLike, priorTransform, nlive, ndims, maxiter):
         self.logLike = logLike
         self.priorTransform = priorTransform
-        self.nDims = nDims
-        self.bounds = bounds
-        self.nlivepoints = nlivepoints
-        self.accepted = 0
-        self.rejected = 0
-        # self.ncall = 0
+        self.nlive = nlive
+        self.ndims = ndims
+        self.maxiter = maxiter
 
-    def sampler(self, accuracy=0.01, maxiter=10000, outputname=None):
-        vectors = []
-        # vector of loglikes
-        loglikes = []
-        # list for dead values
-        deadpoints = []
-        deadloglikes = []
-        deadweights = []
+    def sampling(self):
+        livesamples = []              # Collection of n objects
+        samples = []          # livesamplesects stored for posterior results
+        H    = 0.0            # Information, initially 0
+        logZ =-1e300       # ln(Evidence Z, initially 0)
 
-        for i in range(self.nlivepoints):
-            vectors.append(self.priorTransform(np.random.rand(self.nDims, )))
-            loglikes.append(self.logLike(vectors[i]))
+        # Set prior objects
+        for i in range(self.nlive):
+            livesamples.append(self.sample_from_prior())
 
-        # join loglikes and points
-        df_live = pd.DataFrame()
-        df_live['points'] = vectors
-        df_live['loglikes'] = loglikes
-        print(loglikes)
-        print("data frame", df_live.head())
-        logz = -np.inf
-        # initial prior mass is x0 = 1, z= 0
-        plogx = 0
-        # h = 0
-        for i in range(maxiter):
-            print("\nIteration {}".format(i + 1))
-            # sort points by loglikes
-            df_live.sort_values('loglikes', inplace=True)
-            df_live.reset_index(drop=True, inplace=True)
-            # sleep(0.5)
-            # record the lower loglike,  L_i in the skilling paper
-            lowpoint, lowloglike = df_live.iloc[0]
-            deadloglikes.append(lowloglike)
-            deadpoints.append(lowpoint)
-            # new prior mass X_i in the paper [crude]
-            # x_current = np.exp(-(i + 1) / self.nlivepoints)
-            clogx = -(i + 1) / float(self.nlivepoints)
-            print("logX_i-1: {}, clogXi: {}".format(plogx, clogx))
-            logwi = logsumexp([plogx, clogx], b=[1, -1])
-            print("logw_i: {}".format(logwi))
-            logLwi = lowloglike + logwi
-            deadweights.append(logLwi)
-            # z increment
-            logz = logsumexp([logz, logLwi])
-            print("lowpoint: {}".format(lowpoint))
-            # newpoint, newlike = self.generate_point(lowpoint, lowloglike)
-            newpoint, newlike = self.metropolis(lowpoint, lowloglike, 50)
-            print("new point: {}".format(newpoint))
-            df_live.iloc[0] = newpoint, newlike
-            self.print_func(df_live, logz)
-            plogx = clogx
-            # if outputname:
-            #     strnewpoint = str(newpoint).lstrip('[').rstrip(']')
-            #     f.write("{} {} {}\n".format(logLwi, newlike, strnewpoint))
-            # What is a good value for f?
-            stop = self.stoppingCriteria(df_live['loglikes'].values, clogx, logz, f=accuracy)
-            if stop:
-                break
-        # if outputname:
-        #     f.close()
-        lgsumexplglikes = logsumexp(df_live['loglikes'].values)
-        logzsum = lgsumexplglikes + clogx - np.log(float(self.nlivepoints))
-        logz = logsumexp([logz, logzsum])
-        print("logz : {}".format(logz))
+        # Outermost interval of prior mass
+        logwidth = log(1.0 - exp(-1.0 / self.nlive))
 
-        df_dead = pd.DataFrame()
-        df_dead['points'] = deadpoints
-        df_dead['loglikes'] = deadloglikes
-        # deadweights = np.exp(np.array(deadweights) - logsumexp(np.array(deadweights)))
-        df_dead['weights'] = deadweights
-        self.saveDFtotxt(df_dead, outputname, ext="1")
-        samples = pd.concat([df_dead, df_live], ignore_index=True)
+        # NESTED SAMPLING LOOP ___________________________________________
+        for nest in range(self.maxiter):
 
-        return ({'nlive': self.nlivepoints, 'niter': i, 'samples': samples,
-                 'logwi': logwi, 'logz': logz})
+            # Worst object in collection, with Weight = width * Likelihood
+            worst = 0
+            for i in range(1,self.nlive):
+                if livesamples[i].logL < livesamples[worst].logL:
+                    worst = i
 
+            livesamples[worst].logWt = logwidth + livesamples[worst].logL
 
-    def metropolis(self, ctheta, cloglike, iter):
-        step = 0.01
+            # Update Evidence Z and Information H
+            logZnew = logsumexp([logZ, livesamples[worst].logWt])
 
-        logf = lambda x: self.logLike(x) + self.logPrior(x)
-        # samples = np.zeros((iter, 2))
-        self.accepted = 0
-        self.rejected = 0
+            H = exp(livesamples[worst].logWt - logZnew) * livesamples[worst].logL + \
+                exp(logZ - logZnew) * (H + logZ) - logZnew
+            logZ = logZnew
 
-        for i in range(iter):
-            #propossal dist
-            vstar = ctheta + step * np.random.normal(size=len(ctheta))
-            r = np.random.rand()
-            # q:
-            if logf(vstar) - logf(ctheta) > np.log(r):
-                ctheta = vstar
-                cloglike = self.logLike(ctheta)
-                self.accepted += 1
+            # Posterior samples (optional)
+            samples.append(livesamples[worst])
+
+            # Kill worst object in favour of copy of different survivor
+            if self.nlive>1: # don't kill if n is only 1
+                while True:
+                    copy = int(self.nlive * np.random.random()) % self.nlive  # force 0 <= copy < n
+                    if copy != worst:
+                        break
+
+            logLstar = livesamples[worst].logL       # new likelihood constraint
+            livesamples[worst] = livesamples[copy]          # overwrite worst object
+
+            # Evolve copied object within constraint
+            updated = self.explore(livesamples[worst], logLstar)
+            assert(updated != None) # Make sure explore didn't update in-place
+            livesamples[worst] = updated
+
+            # Shrink interval
+            logwidth -= 1.0 / self.nlive
+
+        # Exit with evidence Z, information H, and optional posterior samples
+        sdev_H = H/log(2.)
+        sdev_logZ = sqrt(H/self.nlive)
+        results = {"samples":samples, "num_iterations":(nest+1), "logZ":logZ,
+                "logZ_sdev":sdev_logZ, "info_nats":H, "info_sdev":sdev_H}
+
+        self.process_results(results)
+
+        return results
+
+    def sample_from_prior(self):
+        Obj = Object()
+        Obj.u = np.random.random()                # uniform in (0,1)
+        Obj.v = np.random.random()                # uniform in (0,1)
+        Obj.x, Obj.y = self.priorTransform([Obj.u, Obj.v])
+        # Obj.x = Obj.u * (10-0) + 0
+        # Obj.y = Obj.v * (6+2) - 2
+        #theta[c]*(bound[1]-bound[0])+bound[0])
+        # Obj.x, Obj.y = priorTransform([Obj.u, Obj.v])             # map to x
+        # Obj.y = 2.0 * Obj.v                    # map to y
+        Obj.logL = self.logLike([Obj.x, Obj.y])
+        return Obj
+
+    def explore(   # Evolve object within likelihood constraint
+        self, Obj,       # Object being evolved
+        logLstar): # Likelihood constraint L > Lstar
+
+        ret = Object()
+        ret.__dict__ = Obj.__dict__.copy()
+        step = 0.1   # Initial guess suitable step-size in (0,1)
+        accept = 0   # # MCMC acceptances
+        reject = 0   # # MCMC rejections
+        Try = Object()          # Trial object
+
+        for m in range(20):  # pre-judged number of steps
+
+            # Trial object
+            Try.u = ret.u + step * (2.*np.random.random() - 1.)  # |move| < step
+            Try.v = ret.v + step * (2.*np.random.random() - 1.)  # |move| < step
+            Try.u -= floor(Try.u)      # wraparound to stay within (0,1)
+            Try.v -= floor(Try.v)      # wraparound to stay within (0,1)
+            Try.x, Try.y = self.priorTransform([Try.u, Try.v])
+            #4.0 * Try.u - 2.0  # map to x
+            #Try.y = 2.0 * Try.v        # map to y
+            Try.logL = self.logLike([Try.x, Try.y])  # trial likelihood value
+
+            # Accept if and only if within hard likelihood constraint
+            if Try.logL > logLstar:
+                ret.__dict__ = Try.__dict__.copy()
+                accept+=1
             else:
-                self.rejected += 1
-             # Refine step-size
-            if( self.accepted > self.rejected ):   step *= np.exp(1.0 / self.accepted)
-            if( self.accepted < self.rejected ):   step /= np.exp(1.0 / self.rejected)
+                reject+=1
 
-        return ctheta, cloglike
+            # Refine step-size to let acceptance ratio converge around 50%
+            if( accept > reject ):   step *= exp(1.0 / accept)
+            if( accept < reject ):   step /= exp(1.0 / reject)
+        return ret
 
-    def logPrior(self, theta):
-        flag = True
-        for i, bound in enumerate(self.bounds):
-            if bound[0] < theta[i] < bound[1]:
-                pass
-            else:
-                flag = False
-                break
-        if flag:
-            return 0.0
-        else:
-            return -np.inf
+    def process_results(self, results):
+        (x,xx) = (0.0, 0.0) # 1st and 2nd moments of x
+        (y,yy) = (0.0, 0.0) # 1st and 2nd moments of y
+        ni = results['num_iterations']
+        samples = results['samples']
+        logZ = results['logZ']
+        for i in range(ni):
+            w = exp(samples[i].logWt - logZ) # Proportional weight
+            x  += w * samples[i].x
+            xx += w * samples[i].x * samples[i].x
+            y  += w * samples[i].y
+            yy += w * samples[i].y * samples[i].y
+        logZ_sdev = results['logZ_sdev']
+        H = results['info_nats']
+        H_sdev = results['info_sdev']
+        print("# iterates: %i"%ni)
+        print("Evidence: ln(Z) = %g +- %g"%(logZ,logZ_sdev))
+        print("Information: H  = %g nats = %g bits"%(H,H/log(2.0)))
+        #print("mean(x) = %9.4f, stddev(x) = %9.4f"%(x, sqrt(xx-x*x)))
+        print("mean(x) = {}, stddev(x) = {}".format(x, np.std(x)))
+        print("mean(x) = {}, stddev(x) = {}".format(y, np.std(y)))
+        #print("mean(y) = %9.4f, stddev(y) = %9.4f"%(y, sqrt(yy-y*y)))
 
-    def stoppingCriteria(self, loglikes, logx, logz, f=0.01):
-        maxloglike = np.max(loglikes)
-        if maxloglike + logx < logz + np.log(f):
-            print("Stopping Criteria reached!")
-            return True
-        else:
-            print("maxloglike+logx: {}, logz+logf: {}".format(
-                maxloglike + logx, logz + np.log(f)))
-            return False
-
-    def print_func(self, df, logz):
-        print("Accepted: {} || Rejected: {} ".format(
-            self.accepted, self.rejected))
-
-        highestpoint, highestlike = df.iloc[self.nlivepoints - 1]
-        print("Parameter estimation : {}".format(highestpoint))
-        print("logLikelihood : {}".format(highestlike))
-        print("log(Z) : {}".format(logz))
-
-    def saveDFtotxt(self, df, outputname, ext='live-birth'):
-        f = open("{}_{}.txt".format(outputname, ext), 'w+')
-        if ext == '1':
-            normws = logsumexp(df['weights'])
-           # print("logaexp weights {}".format(normws))
-        for _, row in df.iterrows():
-            strpoint = "{}".format(row['points']).lstrip('[').rstrip(']').strip(',')
-            strpoint = strpoint.replace(',', '')
-            strpoint = strpoint.replace('  ', ' ')
-            if ext == '1':
-                strrow = "{} {} {}".format(np.exp(row['weights'] - normws), row['loglikes'], strpoint)
-            else:
-                strrow = "{} {} {}".format(strpoint, row['loglikes'], row['birth_contours'])
-            strrow = strrow.replace('  ', ' ')
-            f.write("{}\n".format(strrow))
-        f.close()
-
-class Parameter:
-    def __init__(self, inivalue, bounds, name, LatexName):
-        self.inivalue = inivalue
-        self.bounds = bounds
-        self.name = name
-        self.LatexName = LatexName
-
-    def paramFile(selfs, outputname):
-        pass
