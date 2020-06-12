a2cbd49fcc39e7999587bee0b6707b76c283c7ae
igomezv <neorelativista@gmail.com>
2020-06-12 10:51:26 -0500

    seems working well (posterior without live)

--- nested.py
+++ nested.py
@@ -23,12 +23,13 @@ class nested:
         lvpoints = np.empty((self.nlive, self.ndims), dtype=np.float64)  # real params
         lloglikes = np.empty((self.nlive,), dtype=np.float64)
         # Start nlive points
+        print("Creating first nlive points")
         for i in range(self.nlive):
             lvpoints[i, :] = self.priorTransform(lupoints[i, :])
             lloglikes[i] = self.loglike(lvpoints[i, :])
-            print("{} {}".format(lvpoints[i, :], lloglikes[i]))
-            # logL.append(self.loglike(vpoints[i]))
-
+            print("{} live point created".format(i+1))
+            # print("{} {}".format(lvpoints[i, :], lloglikes[i]))
+        print("live points created")
         # Begin the nested sampling loop
         # s -> saved
         svpoints = []
@@ -47,7 +48,7 @@ class nested:
             px = cx
             # Find worst index (min loglike)
             worst = np.argmin(lloglikes)
-            print("worst:", worst)
+            # print("worst:", worst)
             # log(L*w) = logL + logw
             clogLw = clogw + lloglikes[worst]
             # Increment z += Lw
@@ -59,15 +60,13 @@ class nested:
             slogw.append(clogw)
 
             loglstar = lloglikes[worst]
-            # Kill worst object in favour of copy of different survivor
+            #Kill worst object in favour of copy of different survivor
             while True:
                 copy = np.random.randint(self.nlive)
                 if copy != worst:
                     u = lupoints[copy, :]
-                    print("u", u)
+                    # print("u", u)
                     break
-
-            #upoints[worst], vpoints[worst], logL[worst] = upoints[copy], vpoints[copy], logL[copy]
             nu, nv, nlogl = self.explore(u, loglstar)
             lupoints[worst] = nu
             lvpoints[worst] = nv
@@ -86,7 +85,7 @@ class nested:
                 print("Stopping criteria!")
                 break
             print("{}/{} | worst {} | logz: {:.3f} | dlogz: {:.3f} | logw: {:.3f} "
-                  "| logLstar: {:.3f} | v: {}".format(i + 1, worst, self.maxiter, clogz,
+                  "| logLstar: {:.3f} | v: {}".format(i + 1, self.maxiter, worst, clogz,
                                                       cdlogz, clogw, loglstar, lvpoints[worst]))
 
         # Last increment in Z
@@ -95,30 +94,34 @@ class nested:
         print("Final logZ: {}".format(clogz))
 
         # Postprocessing
-        # plist = np.array(plist)
         normws = logsumexp(np.array(slogLw))
         nLw = np.exp(np.array(slogLw) - normws)
-        # plist[:, 0] = np.exp(plist[:, 0] - normws)
         nsamp, _ = np.shape(svpoints)
-        # f = open("posteriors.txt", "+w")
+        f = open("posteriors.txt", "+w")
         for i in range(nsamp):
-             print("{} {} {}".format(nLw[i], slogL[i], svpoints[i]))
-             #" ".join("{}".format(plist[i]).split()).replace("[", "").replace("]","")
-             #f.write("{}\n".format(strrow))
-        #f.close()
+            strv=""
+            for el, p in enumerate(svpoints[i]):
+                if el==0:
+                    strv = "{}{}".format(strv, p)
+                else:
+                    strv = "{} {}".format(strv, p)
+            f.write("{} {} {}\n".format(nLw[i], slogL[i], strv))
+        f.close()
 
     def explore(self, uworst, logLstar):
         step = 0.1
         accept = 0
         reject = 0
-        for i in range(100):
+        ncall = 0
+        n = 20
+        for _ in range(n):
             while True:
-                pu = uworst + step * np.random.rand(self.ndims)
+                pu = uworst + step * np.random.randn(self.ndims)
                 if np.all(pu > 0.) and np.all(pu < 1.):
-                    break
+                     break
             pv = self.priorTransform(pu)
             ploglike = self.loglike(pv)
-            if ploglike > logLstar:
+            if ploglike >= logLstar:
                 accept += 1
                 break
             else:
