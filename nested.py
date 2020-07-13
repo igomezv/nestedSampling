2f956e4fa9016ad8f3442846619d9922a9b9acb6
igomezv <neorelativista@gmail.com>
2020-07-12 20:30:58 -0500

    removing unnecessary lines and cleaning up code

--- nested.py
+++ nested.py
@@ -5,24 +5,15 @@ import scipy as sc
 
 class nested:
     def __init__(self, loglike, priorTransform, nlive, ndims, maxiter=100000,
-                 outputname="outputs/test", sampletype='diff',
-                 mc_scale=0.0001, ajust_mcstep=False):
-        # mcmc fixed step_size = (0.001-0.01), mcmc_cov step_size = (0.0001-0.001)
-        # self.sampletype = {'mcmc', 'mcmc_cov', 'diff'}
+                 outputname="outputs/test", mc_scale=1.0):
+
         self.loglike = loglike
         self.priorTransform = priorTransform
         self.ndims = ndims
         self.nlive = nlive
         self.maxiter = maxiter
         self.outputname = outputname
-        # Following is for mcmc_explore
-        self.mcaccept = 0
-        self.mcreject = 0
-        self.list_accept = []
-        self.ajust_mcstep = ajust_mcstep
-        # self.scale = 1 # if ajust_scale = True
         self.scale = mc_scale
-        self.sampletype = sampletype
 
     def sampling(self, dlogz=0.01):
         """
@@ -80,15 +71,12 @@ class nested:
 
             loglstar = lloglikes[worst]
 
-            if self.sampletype == 'diff':
-                nu, nv, nlogl = self.diff_evol(lupoints, worst, loglstar)
-            else:
+            idx = worst
+            while idx == worst:
                 idx = np.random.randint(self.nlive) # choose another point to sample from it
-                if self.sampletype == 'mcmc_cov':
-                    livecov = np.cov(lupoints[:, 0], lupoints[:, 1])
-                    nu, nv, nlogl = self.mcmc_explore(lupoints[idx], lvpoints[idx], loglstar, cov=livecov)
-                elif self.sampletype == 'mcmc':
-                    nu, nv, nlogl = self.mcmc_explore(lupoints[idx], lvpoints[idx], loglstar)
+
+            livecov = np.cov(lupoints[:, 0], lupoints[:, 1])
+            nu, nv, nlogl = self.mcmc_explore(lupoints[idx], loglstar, self.scale*livecov)
 
             lupoints[worst] = nu
             lvpoints[worst] = nv
@@ -114,99 +102,45 @@ class nested:
         print("Final logZ: {}".format(clogz))
 
         self.saveFile([svpoints, slogL, slogLstar], type="dead")
-        # # Adding last live points to posterior samples
+        # Adding last live points to posterior samples
         for p in range(self.nlive):
              slogLw.append(lloglw[p])
              slogL.append(lloglikes[p])
              svpoints.append(lvpoints[p])
-        # # Postprocessing
-        #normws = logsumexp(np.array(slogLw))
-        # nLw = np.exp(np.array(slogLw) - normws)
+
+        # p_i -> posterior weights like in Skilling's paper in Section 13
         pi = np.exp(slogL + slogw - clogz)
         self.saveFile([svpoints, slogL, pi], type="post")
 
-        return {'it': i+1, 'logz': clogz, 'dlogz': dlogz, 'loglw': slogLw, 'logw' : slogw,
-                'logl': slogL, 'loglstar' : slogLstar, 'samples' : svpoints}
+        return {'it': i+1, 'logz': clogz, 'dlogz': dlogz, 'loglw': slogLw, 'logw': slogw,
+                'logl': slogL, 'loglstar': slogLstar, 'samples': svpoints}
 
 
-    def diff_evol(self, upoints, worstidx, logLstar):
-        while True:
-            idx1 = np.random.randint(self.nlive)
+    def mcmc_explore(self, uworst, logLstar, cov, naccepted=100):
+        ploglike = logLstar
+        accepted = 0
+        rejected = 0
+
+        while accepted < naccepted:
             while True:
-                idx2 = np.random.randint(self.nlive)
-                if idx2 != idx1:
+                # Generate a candidate point
+                tryu = np.random.multivariate_normal(uworst, cov)
+                # Force that the point lies in [0, 1]
+                if np.all(tryu >= 0.) and np.all(tryu <= 1.):
                     break
-            # Generate a candidate point
-            tryu = upoints[worstidx] + upoints[idx2] - upoints[idx1]
+            # Obtain the respective physical point
             tryv = self.priorTransform(tryu)
+            # Evaluate loglike in the try point
             tryloglike = self.loglike(tryv)
-            if tryloglike >= logLstar:
-                break
+            # acceptance ratio r
+            # if min(tryloglike - ploglike, 0) > np.log(np.random.uniform(0, 1)):
+            # or hard like constrain
+            if tryloglike >= ploglike:
+                accepted += 1
+            else:
+                rejected += 1
 
         return tryu, tryv, tryloglike
-
-    def mcmc_explore(self, uworst, vworst, logLstar, cov=None, nsteps=100):
-        pu = uworst
-        pv = vworst
-        ploglike = logLstar
-        if cov is not None:
-            # take the diagonal of covariance matrix
-            for j, item in enumerate(cov):
-                for k, c in enumerate(item):
-                    if j != k:
-                        cov[j, k] = 0
-            #normalize values of covariance matrix
-            sumcov = np.sum(cov)
-            cov = cov/sumcov
-
-        while(True):
-            if len(self.list_accept) > 0:
-                if self.ajust_mcstep:
-                    m = np.mean(self.list_accept)
-                    if m > 0.5:
-                        self.scale *= np.exp(1. / np.sum(self.list_accept))
-                    else:
-                        self.scale /= np.exp(1. / (len(self.list_accept) - np.sum(self.list_accept)))
-                    self.list_accept = []
-            naccepts = 0
-            for it in range(nsteps):
-                while True:
-                    # Generate a candidate point
-                    if cov is not None:
-                        tryu = np.random.multivariate_normal(uworst, self.scale*cov)
-                    # as in nested_sampling Buchner implementation from github
-                    else:
-                        tryu = uworst + np.random.normal(0, self.scale, size=self.ndims)
-                    # Force that the point lies in [0, 1]
-                    if np.all(tryu >= 0.) and np.all(tryu <= 1.):
-                        break
-                # Obtain the respective physical point
-                # Evaluate loglike in the try point
-                tryv = self.priorTransform(tryu)
-                tryloglike = self.loglike(tryv)
-                #print("current like: {}, try like: {}".format(logLstar, tryloglike))
-                # acceptance ratio r
-                #if min(tryloglike - ploglike, 0) > np.log(np.random.uniform(0, 1)):
-                # or hard like constrain
-                # accept = L > Li or numpy.random.uniform() < exp(L - Li)
-                accept = tryloglike >= ploglike
-                if accept:
-                    ploglike = tryloglike
-                    pu = tryu
-                    pv = tryv
-                    naccepts+=1
-                if self.ajust_mcstep is False:
-                    if tryloglike >= ploglike:
-                        break
-                else:
-                    self.list_accept.append(accept)
-                    if it > 100:
-                        if tryloglike >= ploglike:
-                            break
-            if naccepts > 0:
-                break
-
-        return pu, pv, ploglike
 
 
     def saveFile(self, result, type='live', fname=None):
