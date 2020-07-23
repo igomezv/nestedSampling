2473e25f6cbeee271c712d7e186fb0f9bc6f2792
igomezv <neorelativista@gmail.com>
2020-07-22 23:07:01 -0500

    cleaning comments

--- volume.py
+++ volume.py
@@ -16,32 +16,26 @@ s = nested(logLike, priorTransform, nlive=200, ndims=ndims, outputname="outputs/
 s.sampling()
 samples = NestedSamples(root='outputs/gaussian')
 
-# Problem: 
-# - These do not look gaussian
-# - increasing the number of steps makes them look worse
-samples.plot_2d([0,1])
+samples.plot_2d([0, 1])
 plt.show()
 
-
-last_samp = samples.iloc[-200,:ndims]
+last_samp = samples.iloc[-200, :ndims]
 volume = ((last_samp - 0.5)**2).sum() * np.pi # volume is pi r^2
-volume
 logX = np.log(volume)
+print("logX vol", logX)
 
 logXs = samples.logX(1000)
-for j, i in enumerate(reversed(range(0,len(logXs),100))):
+print("logXs", type(logXs))
+
+for j, i in enumerate(reversed(range(0, len(logXs), 100))):
     logXs.iloc[i].hist(alpha=0.8)
-    last_samp = samples.iloc[i,:ndims]
-    volume = ((last_samp - 0.5)**2).sum() * np.pi # volume is pi r^2
+    last_samp = samples.iloc[i, :ndims]
+    volume = ((last_samp - 0.5)**2).sum() * np.pi
     logX = np.log(volume)
     plt.axvline(logX, color='C%i' % j)
 
 plt.axvline(logX, color='k')
 plt.show()
-
-
-#samples.logX() # Need to finish anesthetic PR https://github.com/williamjameshandley/anesthetic/pull/81
-# Want to check these look the same
 
 # Also should generalise to n-d balls
 # https://en.wikipedia.org/wiki/N-sphere#Volume_and_surface_area
