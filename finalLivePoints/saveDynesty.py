ea1eb803a55ec0e72888c38248778653430f87d4
igomezv <neorelativista@gmail.com>
2020-09-25 04:18:02 -0500

    add .paramnames file

---
+++ finalLivePoints/saveDynesty.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def saveDynestyChain(result, outputname, parslist):
+    f = open(outputname + '.txt', 'w+')
+
+    weights = np.exp(result['logwt'] - result['logz'][-1])
+
+    postsamples = result.samples
+
+    print('\n Number of posterior samples is {}'.format(postsamples.shape[0]))
+
+    for i, sample in enumerate(postsamples):
+        strweights = str(weights[i])
+        strlogl = str(result['logl'][i])
+        strsamples = str(sample).lstrip('[').rstrip(']')
+        row = strweights + ' ' + strlogl + ' ' + strsamples  # + strOLambda
+        nrow = " ".join(row.split())
+        f.write(nrow + '\n')
+
+    f.close()
+
+    fpars = open(outputname + '.paramnames', 'w+')
+
+    for item in parslist:
+        fpars.write(item[0] + '\t\t\t' + item[1] + '\n')
+
+    fpars.close()
