3aa6064a127719e002317c8206eb3007f552f423
igomezv <neorelativista@gmail.com>
2020-09-25 04:17:17 -0500

    fix bounds

--- finalLivePoints/testBayesian.py
+++ finalLivePoints/testBayesian.py
@@ -6,12 +6,15 @@ from dynesty import plotting as dyplot
 import dynesty
 import numpy as np
 import multiprocessing as mp
-
-#
+from saveDynesty import saveDynestyChain
+from getdist import plots, MCSamples, chains
+from getdist import *
 
 # Set npoints and ndims for N sphere
-npoints = 500
+npoints = 100
 ndims = 3
+dim = True
+outputname = 'test_sampling_{}D_sphere_DIM_{}'.format(ndims, dim)
 
 # call NSphere class
 sphere = NSphere(ndims)
@@ -21,20 +24,27 @@ points = sphere.sampling(npoints)
 
 # Obtain likes and vols for each sample
 likes = sphere.logl_for_samples(points)
-
-samples = np.concatenate([points, likes], axis=1)
+# samples = np.concatenate([points, likes], axis=1)
 
 # Define bounds for free parameters
 sigmaLike = 0.3
-logL_bounds = (-4*sigmaLike*ndims, 0)
-alpha_bounds = (0, 1)
-d_bounds = (1, 10)
-logxMax_bounds = (-1e4, 0)
+logL_bounds = [-4*sigmaLike*ndims, 0]
+alpha_bounds = [0, 1]
+d_bounds = [1, 10]
+logxMax_bounds = [0, -1e4]
 bounds_freeDim = [logL_bounds, logxMax_bounds, alpha_bounds, d_bounds]
 bounds_noDim = [logL_bounds, logxMax_bounds, alpha_bounds]
 
+logl = PropossalLogLike(likes, ndims, sigma=sigmaLike)
 # Define a priorTransform
-bounds = bounds_noDim
+if dim:
+    bounds = bounds_freeDim
+    loglike = logl.loglike_freeDim
+else:
+    bounds = bounds_noDim
+    loglike = logl.loglike_noDim
+
+nfreepars = len(bounds)
 
 def priorTransform(theta):
     points = []
@@ -42,18 +52,33 @@ def priorTransform(theta):
         points.append(theta[c]*(bound[1]-bound[0])+bound[0])
     return points
 
-logl = PropossalLogLike(likes, ndims, sigma=sigmaLike)
-
-loglike = logl.loglike_noDim
-# loglike = logl.loglike_freeDim
-
-sampler = dynesty.NestedSampler(loglike, priorTransform, ndims, bound='multi',
+sampler = dynesty.NestedSampler(loglike, priorTransform, nfreepars, bound='multi',
                                 sample='unif', nlive=100)
 sampler.run_nested(dlogz=0.01)
 sampler.results.summary()
 
-# the order is logL, logxMax, alpha, dim
+results = sampler.results
 
-fig, ax = dyplot.cornerplot(sampler.results, color='blue', show_titles=True,
+pars = [['logL', 'logL_{max}'], ['logX', 'logX_{max}'], ['alpha', '\\alpha']]
+
+if dim:
+    pars.append(['dim', 'dim'])
+
+saveDynestyChain(results, "../outputs/{}".format(outputname), pars)
+filename = '../outputs/{}'.format(outputname)
+mcsamplefile = mcsamples.loadMCSamples(filename, ini=None, jobItem=None, no_cache=False)
+g = plots.getSubplotPlotter(width_inch=10,
+                           analysis_settings={'smooth_scale_2D': 0.8,
+                                              'smooth_scale_1D': 0.8})
+g.settings.lab_fontsize = 10
+g.settings.legend_fontsize = 9
+g.settings.axes_fontsize = 9
+g.triangle_plot(mcsamplefile, [par[0] for par in pars],
+                filled=False,
+                shaded=True)
+
+plt.savefig(filename+"_getdist.png")
+# the order is logL, logxMax, alpha, dim
+fig, ax = dyplot.cornerplot(results, color='blue', show_titles=True,
                            quantiles=None)
-plt.show()
+plt.savefig(filename+"_dyplot.png")
